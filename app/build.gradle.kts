faa0c39c42cf378195234592d9742c8f9ef20bc1
Andrey <rogatin-av@ya.ru>
2025-01-20 12:33:46 +0300

    Init gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    id("application")
+    id("checkstyle")
+    id("jacoco")
+    id("io.freefair.lombok") version "8.11"
+    id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
