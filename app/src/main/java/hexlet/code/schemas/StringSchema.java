56ac86c00c76799315da4972925781b3836922d7
Andrey <rogatin-av@ya.ru>
2025-02-20 16:07:08 +0300

    Update StringSchema.java

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -2,27 +2,6 @@ package hexlet.code.schemas;
 
 public class StringSchema extends BaseSchema<String> {
 
-//    private boolean required = false;
-//    private int minLength = -1;
-//    private String contains = "";
-
-//    public boolean isValid(String text) {
-//
-//        if (required && text == null || text.isEmpty()) {
-//            return false;
-//        }
-//
-//        if (minLength > -1 && text.length() < minLength) {
-//            return false;
-//        }
-//
-//        if (!contains.isEmpty() && !text.contains(contains)) {
-//            return false;
-//        }
-//
-//        return true;
-//    }
-
     public StringSchema required() {
         addValidation("required", text -> text != null && !text.isEmpty());
         return this;
