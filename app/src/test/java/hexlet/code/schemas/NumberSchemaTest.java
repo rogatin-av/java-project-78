bc405e866b92d1284ae58bb8e7aa1a146b81bf66
Andrey <rogatin-av@ya.ru>
2025-02-20 16:07:33 +0300

    Update tests

--- app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -1,63 +1,87 @@
 package hexlet.code.schemas;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class NumberSchemaTest {
 
-    @Test
-    void required() {
-        var schema = new NumberSchema();
-
+    @ParameterizedTest
+    @ValueSource(ints = { 5, 0 })
+    void requiredTrue(int testValue) {
+        NumberSchema schema = new NumberSchema();
         assertTrue(schema.isValid(null));
-
         schema.required();
 
-        assertTrue(schema.isValid(5));
-        assertTrue(schema.isValid(0));
+        assertTrue(schema.isValid(testValue));
+    }
+
+    @Test
+    void requiredFalse() {
+        NumberSchema schema = new NumberSchema();
+        schema.required();
 
         assertFalse(schema.isValid(null));
     }
 
-    @Test
-    void positive() {
-        var schema = new NumberSchema();
+    @ParameterizedTest
+    @ValueSource(ints = { 5, 1 })
+    void positiveTrue(int testValue) {
+        NumberSchema schema = new NumberSchema();
         schema.positive();
 
-        assertTrue(schema.isValid(5));
-
-        assertFalse(schema.isValid(0));
-        assertFalse(schema.isValid(-5));
-        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(testValue));
     }
 
-    @Test
-    void range() {
-        var schema = new NumberSchema();
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(ints = { 0, -5 })
+    void positiveFalse(Integer testValue) {
+        NumberSchema schema = new NumberSchema();
+        schema.positive();
+
+        assertFalse(schema.isValid(testValue));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 5, 2, 7 })
+    void rangeTrue(int testValue) {
+        NumberSchema schema = new NumberSchema();
         schema.range(2, 7);
 
-        assertTrue(schema.isValid(5));
-        assertTrue(schema.isValid(2));
-        assertTrue(schema.isValid(7));
-
-        assertFalse(schema.isValid(1));
-        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(testValue));
     }
 
-    @Test
-    void common() {
-        var schema = new NumberSchema();
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(ints = { 1, 8, -5 })
+    void rangeFalse(Integer testValue) {
+        NumberSchema schema = new NumberSchema();
+        schema.range(2, 7);
+
+        assertFalse(schema.isValid(testValue));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 3, 2, 5 })
+    void commonTrue(int testValue) {
+        NumberSchema schema = new NumberSchema();
         schema.range(7, 9).required().positive().range(2, 5);
 
-        assertTrue(schema.isValid(3));
-        assertTrue(schema.isValid(2));
-        assertTrue(schema.isValid(5));
+        assertTrue(schema.isValid(testValue));
+    }
 
-        assertFalse(schema.isValid(1));
-        assertFalse(schema.isValid(8));
-        assertFalse(schema.isValid(-5));
-        assertFalse(schema.isValid(null));
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(ints = { 1, 8, -5 })
+    void commonFalse(Integer testValue) {
+        NumberSchema schema = new NumberSchema();
+        schema.range(7, 9).required().positive().range(2, 5);
+
+        assertFalse(schema.isValid(testValue));
     }
 }
--- app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -1,62 +1,93 @@
 package hexlet.code.schemas;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class StringSchemaTest {
 
-    @Test
-    void required() {
-        var schema = new StringSchema();
-
+    @ParameterizedTest
+    @ValueSource(strings = { "B", "hello" })
+    void requiredTrue(String testValue) {
+        StringSchema schema = new StringSchema();
         assertTrue(schema.isValid(null));
-
         schema.required();
 
-        assertTrue(schema.isValid("text"));
-
-        assertFalse(schema.isValid(""));
-        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(testValue));
     }
 
-    @Test
-    void minLength() {
-        var schema = new StringSchema();
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {""})
+    void requiredFalse(String testValue) {
+        StringSchema schema = new StringSchema();
+        schema.required();
+
+        assertFalse(schema.isValid(testValue));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "text", "big text" })
+    void minLengthTrue(String testValue) {
+        StringSchema schema = new StringSchema();
         schema.minLength(4);
 
-        assertTrue(schema.isValid("text"));
-        assertTrue(schema.isValid("big text"));
-
-        assertFalse(schema.isValid("one"));
-        assertFalse(schema.isValid(""));
-        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(testValue));
     }
 
-    @Test
-    void contains() {
-        var schema = new StringSchema();
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {"one", ""})
+    void minLengthFalse(String testValue) {
+        StringSchema schema = new StringSchema();
+        schema.minLength(4);
+
+        assertFalse(schema.isValid(testValue));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "text", "untextured"})
+    void containsTrue(String testValue) {
+        StringSchema schema = new StringSchema();
         schema.contains("tex");
 
-        assertTrue(schema.isValid("text"));
-        assertTrue(schema.isValid("untextured"));
-
-        assertFalse(schema.isValid(""));
-        assertFalse(schema.isValid("hello world"));
-        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(testValue));
     }
 
-    @Test
-    void common() {
-        var schema = new StringSchema();
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {"hello world", ""})
+    void containsFalse(String testValue) {
+        StringSchema schema = new StringSchema();
+        schema.contains("tex");
+
+        assertFalse(schema.isValid(testValue));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "hello world", "collaboration"})
+    void commonTrue(String testValue) {
+        StringSchema schema = new StringSchema();
+        schema.required().minLength(5).contains("ll").minLength(10);
+
+        assertTrue(schema.isValid(testValue));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {"hello", "text", ""})
+    void commonFalse(String testValue) {
+        StringSchema schema = new StringSchema();
         schema.required().minLength(5).contains("lo").minLength(10);
 
-        assertTrue(schema.isValid("hello world"));
-
-        assertFalse(schema.isValid("hello"));
-        assertFalse(schema.isValid("text"));
-        assertFalse(schema.isValid(""));
-        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(testValue));
     }
 }
